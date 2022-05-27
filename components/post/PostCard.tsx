f075f3f991adec77c7dea301d3a4ca73bd6741a0
jooyeal <jyol1234@gmail.com>
2022-05-27 19:17:47 +0900

    error

--- components/post/PostCard.tsx
+++ components/post/PostCard.tsx
@@ -11,7 +11,7 @@ const PostCard: React.FC<Props> = ({ post }) => {
     <Link href={`/posts/${post.id}`}>
       <div className="cursor-pointer w-60 border rounded-lg shadow-xl">
         <img
-          className="rounded-tr-lg rounded-tl-lg h-40 object-cover"
+          className="rounded-tr-lg rounded-tl-lg w-full h-40 object-cover"
           src={post.thumbnail}
         />
         <div className="h-10 p-2 overflow-hidden text-ellipsis ellipsis font-sourcecode">
--- pages/regist/[id].tsx
+++ pages/regist/[id].tsx
@@ -49,7 +49,6 @@ const Modify: React.FC<Props> = ({ post, id }) => {
         formData
       );
       url = data.url;
-      console.log(url);
     }
     dispatch(
       updatePostApi({
