412c1db165ace214a87cb8cabeda3764a1ce8fc4
jooyeal <jyol1234@gmail.com>
2022-05-27 19:20:19 +0900

    host url modify

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     CLOUDINARY_PRESET: "h3tknacx",
     CLOUDINARY_NAME: "duzum4r81",
-    HOST_URL: "https://yoru-blog-mrxf47t42-jooyeal.vercel.app",
+    HOST_URL: "https://yoru-blog.vercel.app",
   },
   images: {
     domains: ["res.cloudinary.com"],
