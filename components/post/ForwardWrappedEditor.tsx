5de8c64e0a2957a1781e1d930427d28bf83626c3
jooyeal <jyol1234@gmail.com>
2022-05-28 18:06:53 +0900

    highlight languages add

--- components/post/ForwardWrappedEditor.tsx
+++ components/post/ForwardWrappedEditor.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Editor } from "@toast-ui/react-editor";
 import codeSyntax from "@toast-ui/editor-plugin-color-syntax";
-import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all";
+import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
 import Prism from "prismjs";
 import "prismjs/themes/prism.css";
 import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
+import "prismjs/components/prism-swift";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-typescript";
 
 const ForwardWrappedEditor = (props: any) => {
   const { forwardedRef } = props;
--- components/post/ForwardWrappedViewer.tsx
+++ components/post/ForwardWrappedViewer.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Viewer } from "@toast-ui/react-editor";
-import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all";
+import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
 import Prism from "prismjs";
 import "prismjs/themes/prism.css";
 import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
+import "prismjs/components/prism-swift";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-typescript";
 
 const ForwardWrappedViewer = (props: any) => {
   const { forwardedRef } = props;
